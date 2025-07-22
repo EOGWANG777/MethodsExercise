2028b97563229bdbf495aa3dfcb175781a680fec
Esther Ogwang <Estherogwang@gmail.com>
2025-07-22 15:56:34 -0500

    completed C# story program exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -35,7 +35,7 @@ namespace MethodsExercise
             Console.WriteLine("What is your zodiac sign?");
             string zodiacSign = Console.ReadLine();
 
-            Console.WriteLine($"$Hello there! my name is {name} and I really enjoy going for walks, reading books and shopping. At the store like to buy a bouquet of {favFlowers} to enjoy.Currently, the sport I play most is {favoriteSport}. I was born in February, so I am an {zodiacSign} air sign.");
+            Console.WriteLine($"$Hello there! my name is {name} and I really enjoy going for walks, reading books of many genres, and shopping. At the store like to buy a bouquet of {favFlowers} to enjoy.Currently, the sport I play most is {favoriteSport}. I was born in February, so I am an {zodiacSign} air sign.");
      }
 
     }
