78912d73411276c5ee80e948a21b00ef07df221a
Esther Ogwang <Estherogwang@gmail.com>
2025-07-22 21:21:56 -0500

    completed part two of C# story program

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -7,23 +7,83 @@ namespace MethodsExercise
     {
         static void Main(string[] args)
         {
-          //  Madlib();//
-            Madlib2();
+          Madlib();
+            //Madlib2();//
         }
         
         public static void Madlib()
         {
-           // Console.WriteLine("What is your favorite sport?");//
+            Console.WriteLine(Multiply(x1: 10, x2: 20));
+           // int multiplication = Multiply(x1: 10, x2: 20);//
+            //Console.WriteLine(multiplication);//
+            
+            //Console.WriteLine(subtract(x:79, y:32));//
+            int subtraction = (subtract(x: 79, y: 32));
+            Console.WriteLine(subtraction);
+            
+            //Console.WriteLine(add(A:12, B:88));//
+            int addition = (add(A: 12, B: 88));
+            Console.WriteLine(addition);
+            
+            //Console.WriteLine(divide(x2:20, y2:80));//
+            int division = (divide(x2: 20, y2: 80));
+            Console.WriteLine(division);
+            
+            // Console.WriteLine(sum(8, 8, 8, 3));//
+            int addednumbers = sum(8,8,8,3);
+            Console.WriteLine(addednumbers);
+        
+            
+            Console.WriteLine("What is your favorite sport?");
             string favoriteSport = Console.ReadLine();
-           // Console.WriteLine("What is your name?");//
+            Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            //Console.WriteLine($"Hello, {name}! which are your favorite flowers?");//
+            Console.WriteLine($"Hello, {name}! which are your favorite flowers?");
             string favFlowers = Console.ReadLine();
-           // Console.WriteLine("What is your zodiac sign?");//
+            Console.WriteLine("What is your zodiac sign?");
             string zodiacSign = Console.ReadLine();
 
-            //Console.WriteLine($"$Hello there! my name is {name} and I really enjoy going for walks, reading books and shopping. At the store like to buy a bouquet of {favFlowers} to enjoy.Currently, the sport I play most is {favoriteSport}. I was born in February, so I am an {zodiacSign} air sign.");//
+            Console.WriteLine($"$Hello there! my name is {name} and I really enjoy going for walks, reading books and shopping. At the store like to buy a bouquet of {favFlowers} to enjoy.Currently, the sport I play most is {favoriteSport}. I was born in February, so I am an {zodiacSign} air sign.");
         }
+        
+        //Exercise two//
+        public static int Multiply(int x1, int x2)
+
+        {
+            return x1 * x2;
+        }
+
+        public static int subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int add(int A, int B)
+        {
+            return A + B;
+        }
+
+        public static int divide(int x2, int y2)
+
+        {
+            return x2 / y2;
+        }
+
+        public static int sum(params int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                //sum = sum + number;//
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        
+
      public static   void Madlib2()
      {
             Console.WriteLine("What is your favorite sport?");
@@ -37,6 +97,9 @@ namespace MethodsExercise
 
             Console.WriteLine($"$Hello there! my name is {name} and I really enjoy going for walks, reading books of many genres, and shopping. At the store like to buy a bouquet of {favFlowers} to enjoy.Currently, the sport I play most is {favoriteSport}. I was born in February, so I am an {zodiacSign} air sign.");
      }
+     
+
+
 
     }
     
--- MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e800859b85a8dc4830854ffab9ed5f559fa227dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
